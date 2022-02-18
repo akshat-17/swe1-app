b70c3668a817947fa8b69082bf3767d7efcd0987
Akshat Shah <as15098@nyu.edu>
2022-02-18 18:59:19 -0500

    Changed settings file

--- mypolls/urls.py
+++ mypolls/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 # from django.conf.urls import url
 
 urlpatterns = [
-    #  path('polls/', include('polls.urls')),
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
