158d89195cf2880e8c9d44b52a8c903eafb3d36c
akshat-17 <60538557+akshat-17@users.noreply.github.com>
2022-03-09 00:03:12 -0500

    Final commit hopefully!

--- mypolls/urls.py
+++ mypolls/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 # from django.conf.urls import url
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
